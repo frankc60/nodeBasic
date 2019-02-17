922ace1c811c8f0dc3c222214735cbbd1155ad56
frankc60 <frankc60@gmail.com>
2019-02-17 16:25:10 +0100

    text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 //create a server object:
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello World!'); //write a response to the client
+    res.write('Hello World! this is a test'); //write a response to the client
     res.end(); //end the response
     console.log("hello world requested!!");
 }).listen(5000); //the server object listens on port 8080
